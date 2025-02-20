1b0a9f42094063a5cf360fce734b752c3582f65c
Cristian727 <cristian.gonzalez7@hotmail.com>
2025-02-20 13:03:13 +0100

    FuncionaTodo

--- Oauth/server.js
+++ Oauth/server.js
@@ -3,12 +3,15 @@ const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
 const GitHubStrategy = require('passport-github2').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 const port = 3000;
 
 const CLIENT_ID_GITHUB = process.env.CLIENT_ID_GITHUB;
 const CLIENT_SECRET_GITHUB = process.env.CLIENT_SECRET_GITHUB;
+const CLIENT_ID_GOOGLE = process.env.CLIENT_ID_GOOGLE;
+const CLIENT_SECRET_GOOGLE = process.env.CLIENT_SECRET_GOOGLE;
 
 
 passport.use(new GitHubStrategy({
@@ -20,6 +23,16 @@ function(accessToken, refreshToken, profile, done) {
   return done(null, profile);
 }
 ));
+
+passport.use(new GoogleStrategy({
+    clientID: CLIENT_ID_GOOGLE,
+    clientSecret: CLIENT_SECRET_GOOGLE,
+    callbackURL: 'http://localhost:3000/auth/google/callback'
+  },
+  function(accessToken, refreshToken, profile, done) {
+    return done(null, profile);
+  }
+  ));
 
 passport.serializeUser(function(user, done) {
   done(null, user);
@@ -38,32 +51,59 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/');
+}
+
 app.get("/", (req, res) => {
-  const html = "<a href='/auth/github'>Autenticar con GitHub</a>";
+    const html = `
+      <a href='/auth/github'>Autenticar con GitHub</a><br>
+      <a href='/auth/google'>Autenticar con Google</a>
+    `;
     res.send(html);
-});
+  });
+
 
 app.get('/auth/github',
-    passport.authenticate('github', { scope: ['user:email']})
-)
+  passport.authenticate('github', { scope: [ 'user:email' ] }),
+  
+);
+
+
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ['profile', 'email'] })
+  );
 
 app.get('/auth/github/callback',
-    passport.authenticate('github', {failureRedirect: '/' }),
-    (req,res) => {
-        res.redirect('/profile');
-    })
+    passport.authenticate('github', { failureRedirect: '/' }),
+    (req, res, next) => {
+        res.send('/profile');
+    }
+);
 
-// Ruta principal
-app.get('/', (req, res) => {
-  res.send('¡Hola, mundo!');
+app.get('/auth/google/callback',
+    passport.authenticate('google', { failureRedirect: '/' }),
+    (req, res) => {
+      res.redirect('/profile');
+    }
+  );
+
+app.get('/profile', ensureAuthenticated, (req, res) => {
+    res.send(`Hola ${req.user.username || req.user.displayName}`);
 });
 
-app.get('/profile', (req, res) => {
-    res.send(`Hola`)
-})
+app.get('/logout', (req, res) => {
+    req.logout(done => {
+        console.log('Usuario deslogueado');
+    });
+    res.redirect('/');
+});
 
 
-// Iniciar el servidor
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
 });
